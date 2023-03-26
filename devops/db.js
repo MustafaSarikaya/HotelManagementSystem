df200fc75c52a989e27445ce6b9389583e62c7c8
MustafaSarikaya <msarikay@uottawa.ca>
2023-03-26 18:04:52 -0400

    set-up env variables

--- devops/db.js
+++ devops/db.js
@@ -1,10 +1,11 @@
 import mariadb from 'mariadb'
+require('dotenv').config()
 
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Password123!',
-    database: 'HMS_database'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
 export default pool
